8ca43d1dd0051f8f4a375dbfcc21c3af84e83976
root <root@56_102.localdomain>
2023-05-15 23:28:46 +0800

    0515 birthday

--- wxcloudrun/static/script/zepto.weui.js
+++ wxcloudrun/static/script/zepto.weui.js
@@ -3673,12 +3673,8 @@ Device/OS Detection
 
     var Swipeout = function(el) {
         this.container = $(el);
-        console.log(this)
-        this.mover = this.container.find('.weui-cell__bd')
-        console.log(this.mover)
+        this.mover = this.container.find('>.weui-cell__bd')
         this.attachEvents();
-        console.log('touch')
-        console.log(this.attachEvents())
         cache.push(this)
     }
 
@@ -3692,10 +3688,7 @@ Device/OS Detection
         if (transform && transform.length) this.startX = parseInt(transform[4])
         this.diffX = this.diffY = 0;
         this._closeOthers()
-        console.log(this.container)
-        this.limit = this.container.find('.weui-cell__ft').width() || 68; // 因为有的时候初始化的时候元素是隐藏的（比如在对话框内），所以在touchstart的时候计算宽度而不是初始化的时候
-        console.log("limit")
-        console.log(this.limit)
+        this.limit = this.container.find('>.weui-cell__ft').width() || 68; // 因为有的时候初始化的时候元素是隐藏的（比如在对话框内），所以在touchstart的时候计算宽度而不是初始化的时候
     };
 
     Swipeout.prototype.touchMove= function(e) {
